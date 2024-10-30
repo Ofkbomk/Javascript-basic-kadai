abd50781fbd1473f2e835313940b6d3f592ac781
Ofkbomk <kmeiofkbo@gmail.com>
2024-10-30 14:15:27 +0900

    kadai_020を追加

---
+++ kadai_020/event.js
@@ -0,0 +1,10 @@
+//ボタン
+const btn = document.getElementById('btn');
+
+//テキスト
+const txt = document.getElementById('text');
+
+//ボタンがクリックされた時の処理
+btn.addEventListener('click', () => {
+  text.textContent = 'ボタンをクリックしました';
+});
